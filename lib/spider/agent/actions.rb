5908581935df1ed300f0704acdd932ac25d43165
krishpranav <krisna.pranav@gmail.com>
2021-07-18 12:19:32 +0530

    lib: spider agent: actions

--- lib/spider/agent/actions.rb
+++ lib/spider/agent/actions.rb
@@ -1,35 +1,50 @@
 module Spider
     class Agent
-        module Actions
+      module Actions
 
-            class Actions RuntimeError
-            end
-
-            class Paused < Action
-            end
-
-            class SkipLink < Action
-            end
-
-            class SkipPage < Action
-            end
+        class Action < RuntimeError
         end
-
-        def continue!(&block)
-            @paused = false
-            return run(&block)
+  
+        class Paused < Action
         end
-
-        def pause=(state)
-            @paused = state
+  
+        class SkipLink < Action
         end
-
-        def pause!
-            @paused = true
-            raise(Actions::Paused)
+  
+        class SkipPage < Action
         end
-
-        def paused?
-            @paused == true
-        end
-        
+      end
+  
+      def continue!(&block)
+        @paused = false
+        return run(&block)
+      end
+  
+      def pause=(state)
+        @paused = state
+      end
+  
+      def pause!
+        @paused = true
+        raise(Actions::Paused)
+      end
+  
+      def paused?
+        @paused == true
+      end
+  
+      def skip_link!
+        raise(Actions::SkipLink)
+      end
+  
+      def skip_page!
+        raise(Actions::SkipPage)
+      end
+  
+      protected
+  
+      def initialize_actions(options={})
+        @paused = false
+      end
+    end
+  end
