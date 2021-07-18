73f768d3bc19c56c0a55c52db0f827591e668da0
krishpranav <krisna.pranav@gmail.com>
2021-07-18 13:28:53 +0530

    lib: spider agent robots

--- lib/spider/agent/robots.rb
+++ lib/spider/agent/robots.rb
@@ -1,16 +1,25 @@
 begin
     require 'robots'
-rescue LoadError
-end
-
-module Spider
+  rescue LoadError
+  end
+  
+  module Spider
     class Agent
-        
-        def initialize_robots
-            unless Object.const_defined?(:Robots)
-                raise(ArgumentError,":robots option given but unable to require 'robots' gem")
-            end
 
-            @robots = Robots.new(@user_agent)
+      def initialize_robots
+        unless Object.const_defined?(:Robots)
+          raise(ArgumentError,":robots option given but unable to require 'robots' gem")
         end
-        
+  
+        @robots = Robots.new(@user_agent)
+      end
+  
+      def robot_allowed?(url)
+        if @robots
+          @robots.allowed?(url)
+        else
+          true
+        end
+      end
+    end
+  end
