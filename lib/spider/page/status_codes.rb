0b86b4317a48fff1fe218ca9527727f1139dfc44
krishpranav <krisna.pranav@gmail.com>
2021-07-18 18:09:16 +0530

    lib: spider page status codes

--- lib/spider/page/status_codes.rb
+++ lib/spider/page/status_codes.rb
@@ -1,44 +1,58 @@
 module Spider
     class Page
-        
-        def code
-            @response.code.to_i
+
+      def code
+        @response.code.to_i
+      end
+  
+      def is_ok?
+        code == 200
+      end
+  
+      alias ok? is_ok?
+  
+      def timedout?
+        code == 308
+      end
+  
+      def bad_request?
+        code == 400
+      end
+  
+      def is_unauthorized?
+        code == 401
+      end
+  
+      alias unauthorized? is_unauthorized?
+  
+
+      def is_forbidden?
+        code == 403
+      end
+  
+      alias forbidden? is_forbidden?
+  
+      def is_missing?
+        code == 404
+      end
+  
+      alias missing? is_missing?
+  
+      def had_internal_server_error?
+        code == 500
+      end
+  
+      def is_redirect?
+        case code
+        when 300..303, 307
+          true
+        when 200
+          meta_redirect?
+        else
+          false
         end
-
-        def is_ok?
-            code == 200
-        end
-
-        alias ok? is_ok?
-
-
-        def timeout?
-            code == 300 
-        end
-
-        def bad_request?
-            code == 400
-        end
-
-        def is_unauthorized?
-            code == 401
-        end
-
-        alias unauthorized? is_unauthorized?
-
-        def is_forbidden?
-            code == 403
-        end
-
-        alias forbidden? is_forbidden?
-
-        def is_missing?
-            code == 404
-        end
-
-        alias missing? is_missing?
-
-        def had_internal_server_error?
-            code == 500
-        end
-        
+      end
+  
+      alias redirect? is_redirect?
+    end
+  end
