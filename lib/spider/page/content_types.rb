7015c7044c157fb061eaf3f1635764c234197d8f
krishpranav <krisna.pranav@gmail.com>
2021-07-18 17:32:22 +0530

    lib: spider page content_types[directory, html]

--- lib/spider/page/content_types.rb
+++ lib/spider/page/content_types.rb
@@ -24,4 +24,33 @@ module Spider
 
             return nil 
         end
+
+        def is_content_type?(type)
+            if type.include?('/')
+
+                content_types.any? do |value|
+                    value = value.split(';',2).first
+
+                    value == type
+                end
+
+            else
+                content_types.any? do |value|
+                    value = value.split(';',2).first
+                    value = value.split('/',2).last
+
+                    value == type
+                end
+            end
+        end
+
+        def plain_text?
+            is_content_type?('text/plain')
+        end
+
+        alias txt? plain_text?
+
+        def directory?
+            is_content_type?('text/directory')
+        end
         
