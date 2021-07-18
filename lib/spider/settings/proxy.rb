04e05d29f606a88a29c6a60b1c1b8affc52c8b02
krishpranav <krisna.pranav@gmail.com>
2021-07-18 18:21:38 +0530

    lib: spider settings proxy

---
+++ lib/spider/settings/proxy.rb
@@ -0,0 +1,20 @@
+require 'spider/proxy'
+
+module Spider
+    module Settings
+
+        module Proxy
+
+            def proxy
+                @proxy ||= Spider::Proxy.new
+            end
+
+            def proxy=(new_proxy)
+                @proxy = case new_proxy
+                         when Spidr::Proxy then new_proxy
+                         when Hash         then Spidr::Proxy.new(new_proxy)
+                         when nil          then Spidr::Proxy.new
+                         else
+                           raise(TypeError,"#{self.class}#{__method__} only accepts Proxy, Hash or nil")
+                         end
+              end
