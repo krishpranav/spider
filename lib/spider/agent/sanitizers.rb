31c79683e45c1e3e20cea11f38e1f14ea141e79b
krishpranav <krisna.pranav@gmail.com>
2021-07-18 13:31:29 +0530

    lib: spider agent sanitizers

--- lib/spider/agent/sanitizers.rb
+++ lib/spider/agent/sanitizers.rb
@@ -1,18 +1,26 @@
 require 'uri'
 
 module Spider
-    class Agent
-        
-        attr_accessor :strip_fragments
-        attr_accessor :strip_query
-        
-        def sanitize_url(url)
-            url = URI(url)
+  class Agent
 
-            url.fragment = nil if @strip_fragments
-            url.query = nil if @strip_query
+    attr_accessor :strip_fragments
+    attr_accessor :strip_query
 
-            return url
-        end
+    def sanitize_url(url)
+      url = URI(url)
 
-        protected 
+      url.fragment = nil if @strip_fragments
+      url.query    = nil if @strip_query
+
+      return url
+    end
+
+    protected
+
+    def initialize_sanitizers(options={})
+      @strip_fragments = options.fetch(:strip_fragments,true)
+      @strip_query     = options.fetch(:strip_query,false)
+    end
+
+  end
+end
