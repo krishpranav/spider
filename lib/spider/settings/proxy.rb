f14eb92f880c5b60e5c4de39a1f2ddac00ef783e
krishpranav <krisna.pranav@gmail.com>
2021-07-18 18:22:31 +0530

    lib: spider setetings proxy

--- lib/spider/settings/proxy.rb
+++ lib/spider/settings/proxy.rb
@@ -1,20 +1,27 @@
 require 'spider/proxy'
 
 module Spider
-    module Settings
+  module Settings
+    module Proxy
 
-        module Proxy
+      def proxy
+        @proxy ||= Spidr::Proxy.new
+      end
 
-            def proxy
-                @proxy ||= Spider::Proxy.new
-            end
+      def proxy=(new_proxy)
+        @proxy = case new_proxy
+                 when Spidr::Proxy then new_proxy
+                 when Hash         then Spidr::Proxy.new(new_proxy)
+                 when nil          then Spidr::Proxy.new
+                 else
+                   raise(TypeError,"#{self.class}#{__method__} only accepts Proxy, Hash or nil")
+                 end
+      end
 
-            def proxy=(new_proxy)
-                @proxy = case new_proxy
-                         when Spidr::Proxy then new_proxy
-                         when Hash         then Spidr::Proxy.new(new_proxy)
-                         when nil          then Spidr::Proxy.new
-                         else
-                           raise(TypeError,"#{self.class}#{__method__} only accepts Proxy, Hash or nil")
-                         end
-              end
+      def disable_proxy!
+        @proxy = Spidr::Proxy.new
+        return true
+      end
+    end
+  end
+end
