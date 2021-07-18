1cf2b660df7383ef60aa9e2dae63f4e5ae985f8b
krishpranav <krisna.pranav@gmail.com>
2021-07-18 12:16:42 +0530

    lib: spider agent actions[continue, pause, paused?]

--- lib/spider/agent/actions.rb
+++ lib/spider/agent/actions.rb
@@ -14,4 +14,22 @@ module Spider
             class SkipPage < Action
             end
         end
+
+        def continue!(&block)
+            @paused = false
+            return run(&block)
+        end
+
+        def pause=(state)
+            @paused = state
+        end
+
+        def pause!
+            @paused = true
+            raise(Actions::Paused)
+        end
+
+        def paused?
+            @paused == true
+        end
         
