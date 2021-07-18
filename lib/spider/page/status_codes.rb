98fab51021afe595f74216555fc84369a255048a
krishpranav <krisna.pranav@gmail.com>
2021-07-18 18:07:57 +0530

    lib: spider page status codes

--- lib/spider/page/status_codes.rb
+++ lib/spider/page/status_codes.rb
@@ -15,4 +15,30 @@ module Spider
         def timeout?
             code == 300 
         end
+
+        def bad_request?
+            code == 400
+        end
+
+        def is_unauthorized?
+            code == 401
+        end
+
+        alias unauthorized? is_unauthorized?
+
+        def is_forbidden?
+            code == 403
+        end
+
+        alias forbidden? is_forbidden?
+
+        def is_missing?
+            code == 404
+        end
+
+        alias missing? is_missing?
+
+        def had_internal_server_error?
+            code == 500
+        end
         
