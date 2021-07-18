303e3494daee1a9d04a99dbd86057f1521eb11e1
krishpranav <krisna.pranav@gmail.com>
2021-07-18 17:41:46 +0530

    lib: spider page cookies

--- lib/spider/page/cookies.rb
+++ lib/spider/page/cookies.rb
@@ -1,18 +1,37 @@
 require 'set'
 
 module Spider
-    class Page
+  class Page
+    RESERVED_COOKIE_NAMES = /^(?:Path|Expires|Domain|Secure|HTTPOnly)$/i
 
-        RESERVED_COOKIES_NAMES = /^(?:Path|Expires|Domain|Secure|HTTPOnly)$/i
 
-        def cookie
-            @response['Set-Cokkie'] || ''
-        end
-
-        alias raw_cookie cookie
-
-        def cookies
-            (@response.get_fields('Set-Cookie') || [])
-        end
+    def cookie
+      @response['Set-Cookie'] || ''
     end
+
+    alias raw_cookie cookie
+
+    def cookies
+      (@response.get_fields('Set-Cookie') || [])
+    end
+
+
+    def cookie_params
+      params = {}
+
+      cookies.each do |value|
+        value.split(';').each do |param|
+          param.strip!
+
+          name, value = param.split('=',2)
+
+          unless name =~ RESERVED_COOKIE_NAMES
+            params[name] = (value || '')
+          end
+        end
+      end
+
+      return params
+    end
+  end
 end
