7d9eb37e53330bf40c238edd4751f118e3ea8a0f
krishpranav <krisna.pranav@gmail.com>
2021-07-19 09:02:25 +0530

    lib: spider cookie jar

--- lib/spider/cookie_jar.rb
+++ lib/spider/cookie_jar.rb
@@ -2,45 +2,104 @@ require 'spider/page'
 require 'set'
 
 module Spider
-    class CookieJar
-        include Enumerable
 
-        def initialize
-            @params = {}
-            @dirty = Set[]
-            @cookies = {}
+  class CookieJar
+
+    include Enumerable
+
+    def initialize
+      @params = {}
+
+      @dirty   = Set[]
+      @cookies = {}
+    end
+
+    def each(&block)
+      @params.each(&block)
+    end
+
+    def [](host)
+      @params[host] ||= {}
+    end
+
+
+    def []=(host,cookies)
+      collected = self[host]
+
+      cookies.each do |key,value|
+        if collected[key] != value
+          collected.merge!(cookies)
+          @dirty << host
+
+          break
+        end
+      end
+
+      return cookies
+    end
+
+    def from_page(page)
+      cookies = page.cookie_params
+
+      unless cookies.empty?
+        self[page.url.host] = cookies
+        return true
+      end
+
+      return false
+    end
+
+    def for_host(host)
+      if @dirty.include?(host)
+        values = []
+
+        cookies_for_host(host).each do |name,value|
+          values << "#{name}=#{value}"
         end
 
-        def each(&block)
-            @params.each(&block)
-        end
+        @cookies[host] = values.join('; ')
+        @dirty.delete(host)
+      end
 
-        def [](host)
-            @params[host] || = {}
-        end
-        
-        
-        def []=(host, cookies)
-            collected = self[host]
+      return @cookies[host]
+    end
 
-            cookies.each do |key,value|
-                if collected[key] != value
-                    collected.merge!(cookies)
-                    @dirty << host
-                break
-                end
+    def cookies_for_host(host)
+      host_cookies = (@params[host] || {})
+      sub_domains  = host.split('.')
+
+      while sub_domains.length > 2
+        sub_domains.shift
+
+        if (parent_cookies = @params[sub_domains.join('.')])
+          parent_cookies.each do |name,value|
+
+            unless host_cookies.has_key?(name)
+              host_cookies[name] = value
             end
-        return cookies
+          end
         end
+      end
 
-        def from_page(page)
-            cookies = page.cookie_params
+      return host_cookies
+    end
 
-            unless cookies.empty?
-                self[page.url.host] = cookies
-                return true
-            end
 
-            return false
-        end
-        
+    def clear!
+      @params.clear
+
+      @dirty.clear
+      @cookies.clear
+      return self
+    end
+
+    def size
+      @params.size
+    end
+
+    def inspect
+      "#<#{self.class}: #{@params.inspect}>"
+    end
+
+  end
+end
